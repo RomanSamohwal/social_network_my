bd5b30f41d68e27ea645c2ba48786e26119db3ba
RomanSamohwal <rsamohwal@gmail.com>
2020-09-30 05:11:28 +0300

    Updates

---
+++ precache-manifest.ade81b857a107f4c173c1267316d12f5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad872618e45f85ca11e9424abb00ff19",
+    "url": "/social_network_my/index.html"
+  },
+  {
+    "revision": "e8de1f9661fbf76cf063",
+    "url": "/social_network_my/static/css/3.3764e081.chunk.css"
+  },
+  {
+    "revision": "b2d32c5f35e8c9c72994",
+    "url": "/social_network_my/static/css/4.869b8996.chunk.css"
+  },
+  {
+    "revision": "d5083de706e793590abf",
+    "url": "/social_network_my/static/css/main.af5bd495.chunk.css"
+  },
+  {
+    "revision": "0788d0fdc6f0f29e0ca5",
+    "url": "/social_network_my/static/js/2.78f6b29a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/social_network_my/static/js/2.78f6b29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8de1f9661fbf76cf063",
+    "url": "/social_network_my/static/js/3.acf83a80.chunk.js"
+  },
+  {
+    "revision": "b2d32c5f35e8c9c72994",
+    "url": "/social_network_my/static/js/4.583e2d71.chunk.js"
+  },
+  {
+    "revision": "d5083de706e793590abf",
+    "url": "/social_network_my/static/js/main.860fc126.chunk.js"
+  },
+  {
+    "revision": "75e7e7f5d6ee9b57de82",
+    "url": "/social_network_my/static/js/runtime-main.f5b04333.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social_network_my/precache-manifest.fcf7c5416bb1ef76a05dc576879035ca.js"
+  "/social_network_my/precache-manifest.ade81b857a107f4c173c1267316d12f5.js"
 );
 
 self.addEventListener('message', (event) => {
